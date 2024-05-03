168605fc8ae0062ca2c66a065c0a1728e7a9e2f5
Sávio Marques de Souza <saviomarquessouza@gmail.com>
2024-05-03 10:08:14 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/projectwebservices/project/config/TestConfig.java
+++ src/main/java/com/projectwebservices/project/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.projectwebservices.project.entities.Category;
 import com.projectwebservices.project.entities.Order;
+import com.projectwebservices.project.entities.OrderItem;
 import com.projectwebservices.project.entities.Product;
 import com.projectwebservices.project.entities.User;
 import com.projectwebservices.project.entities.enums.OrderStatus;
 import com.projectwebservices.project.repositories.CategoryRepository;
+import com.projectwebservices.project.repositories.OrderItemRepository;
 import com.projectwebservices.project.repositories.OrderRepository;
 import com.projectwebservices.project.repositories.ProductRepository;
 import com.projectwebservices.project.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository ProductRepository; 
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -67,7 +72,14 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
 		
 	}
--- src/main/java/com/projectwebservices/project/entities/Product.java
+++ src/main/java/com/projectwebservices/project/entities/Product.java
@@ -18,7 +18,6 @@ import jakarta.persistence.Table;
 @Table(name = "tb_product")
 public class Product implements Serializable {
 
-
 	private static final long serialVersionUID = 1L;
 	
 	@Id
