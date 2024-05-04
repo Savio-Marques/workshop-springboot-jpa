45915d8ff3de198a98a62d967256a410f3cb85e1
Sávio Marques de Souza <saviomarquessouza@gmail.com>
2024-05-04 10:00:57 -0300

    Payment, OneToOne association

--- src/main/java/com/projectwebservices/project/config/TestConfig.java
+++ src/main/java/com/projectwebservices/project/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projectwebservices.project.entities.Category;
 import com.projectwebservices.project.entities.Order;
 import com.projectwebservices.project.entities.OrderItem;
+import com.projectwebservices.project.entities.Payment;
 import com.projectwebservices.project.entities.Product;
 import com.projectwebservices.project.entities.User;
 import com.projectwebservices.project.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 	
